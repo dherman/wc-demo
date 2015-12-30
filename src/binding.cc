091cccb99abeaa9bfb9a4db9f32eb605ab3a08d2
Dave Herman <dherman@mozilla.com>
2015-12-29 23:47:25 -0800

    update to latest neon structure

---
+++ src/binding.cc
@@ -0,0 +1,13 @@
+/* THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT. */
+
+#include <nan.h>
+#include <stdio.h>
+
+extern "C" void node_main();
+extern "C" void neon_init(v8::Local<v8::Object> module, void *f);
+
+void __neon_main__(v8::Local<v8::Object> module) {
+  neon_init(module, (void *)node_main);
+}
+
+NODE_MODULE(wc_demo, __neon_main__)
